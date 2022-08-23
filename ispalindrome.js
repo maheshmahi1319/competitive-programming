8272822ea8d6d18e163ea12fc5158cef1c066747
Earth <mahesh.kasodhan@adglobal360.com>
2022-08-23 15:04:39 +0530

    add

--- ispalindrome.js
+++ ispalindrome.js
@@ -5,3 +5,20 @@ function isPalindrome(str){
 }
 
 console.log(isPalindrome(100))
+
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+console.log(checkPalindrome(100))
--- nextpalindrome.js
+++ nextpalindrome.js
@@ -1,47 +1,70 @@
 
-// Javascript program for the above approach
+// // Javascript program for the above approach
 
-	// Function to check whether number is palindrome or not
-	function isPalindrome(num)
-	{
+// 	// Function to check whether number is palindrome or not
+// 	function isPalindrome(num)
+// 	{
 	
-		// Declaring variables
-		let n, k, rev = 0;
+// 		// Declaring variables
+// 		let n, k, rev = 0;
 	
-		// storing num in n so that we can compare it later
-		n = num;
+// 		// storing num in n so that we can compare it later
+// 		n = num;
 	
-		// while num is not 0 we find its reverse and store
-		// in rev
-		while (num != 0) {
-			k = num % 10;
-			rev = (rev * 10) + k;
-			num = Math.floor(num / 10);
-		}
+// 		// while num is not 0 we find its reverse and store
+// 		// in rev
+// 		while (num != 0) {
+// 			k = num % 10;
+// 			rev = (rev * 10) + k;
+// 			num = Math.floor(num / 10);
+// 		}
 	
-		// check if num and its reverse are same
-		if (n == rev) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
+// 		// check if num and its reverse are same
+// 		if (n == rev) {
+// 			return 1;
+// 		}
+// 		else {
+// 			return 0;
+// 		}
+// 	}
 
-// Driver Code
+// // Driver Code
 
-	// Take any number to find its next palindrome
-		// number
-		let num = 9687;
-	console.log(isPalindrome(num))
-		// If number is not Palindrome we go to the next
-		// number using while loop
-		// while (isPalindrome(num) == 0) {
-		// 	num = num + 1;
-		// }
+// 	// Take any number to find its next palindrome
+// 		// number
+// 		let num = 9687;
+// 	console.log(isPalindrome(num))
+// 		// If number is not Palindrome we go to the next
+// 		// number using while loop
+// 		// while (isPalindrome(num) == 0) {
+// 		// 	num = num + 1;
+// 		// }
 	
-		// now we get the next Palindrome so let's print it
-		// document.write("Next Palindrome :");
-		// document.write(num);
+// 		// now we get the next Palindrome so let's print it
+// 		// document.write("Next Palindrome :");
+// 		// document.write(num);
 
-// This code is contributed by splevel62.
+// // This code is contributed by splevel62.
+
+
+function reverse(nums) {
+    var reverse = "";
+    for (var i = String(nums).length - 1; i >= 0; i--) {
+      reverse += String(nums)[i];
+    }
+    return reverse;
+  }
+  
+  function Palindrome(num) {
+    if (String(num).length < 2) {
+      return num + 1
+    }
+  
+    for (var i = num + 1; ; i++) {
+      if (String(i) === reverse(i)) {
+        return i
+      }
+    }
+  }
+  
+  console.log(Palindrome(23))
